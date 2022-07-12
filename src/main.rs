f94474929af2481e6d0fa5f894c3850e3a8f2e75
ThibFrgsGmz <farges.thibault@gmail.com>
2022-07-12 23:04:13 +0200

    Borrowing/Ownership on pointer

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,10 @@
 pub(crate) fn main() {
-    let my_variable= 12;
-    let a_test: i32 = 48u8 as i32;
+    let mut a = String::from("Test");
 
-    let _my_string: &str = "String_1";
-    let _my_string_2: String = String::from("String_2");
-    let mut _my_mutable_string: String = String::from("String_3");
-
-    _my_mutable_string.push_str("HelloThere");
-
-    println!("{my_variable}");
-    println!(r#"{a_test}"#);
-    println!("{_my_mutable_string}");
+    {
+        let b = &mut a;
+        b.push_str(" World!");
+    }
+    let _c = &a;
+    println!("{a}");
 }
