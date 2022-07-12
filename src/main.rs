0f1811f57b07febfc5c64daff2549ccee36257c5
ThibFrgsGmz <farges.thibault@gmail.com>
2022-07-12 21:36:12 +0200

    String manipulation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 pub(crate) fn main() {
-    println!("Hello, world!");
+    let my_variable= 12;
+    let a_test: i32 = 48u8 as i32;
+
+    let _my_string: &str = "String_1";
+    let _my_string_2: String = String::from("String_2");
+    let mut _my_mutable_string: String = String::from("String_3");
+
+    _my_mutable_string.push_str("HelloThere");
+
+    println!("{my_variable}");
+    println!(r#"{a_test}"#);
+    println!("{_my_mutable_string}");
 }
