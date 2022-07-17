919be5a12f7823d1a7a226fde154f38e83a0d64c
ThibFrgsGmz <farges.thibault@gmail.com>
2022-07-17 18:04:07 +0200

    Im/mutable Arrays

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 pub(crate) fn main() {
-    let mut a = String::from("Test");
 
-    {
-        let b = &mut a;
-        b.push_str(" World!");
-    }
-    let _c = &a;
-    println!("{a}");
+    let immutable_array: [u8;4]=[1,2,3,4];
+    let mut mutable_array: [u8;4]=[1,2,3,4];
+
+    // immutable_array[0] = 5; #error `immutable_array` is not declared as mutable
+    mutable_array[0] = 6;
+
+    println!("Here is the immutable array: {immutable_array:?}");
+    println!("Here is the mutable array: {mutable_array:?}");
+    println!("Here is the mutable array: {}", mutable_array.len());
 }
