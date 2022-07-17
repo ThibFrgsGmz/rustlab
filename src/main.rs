29df2e75cd90efda005458e83a8e3ed9b59cafc8
ThibFrgsGmz <farges.thibault@gmail.com>
2022-07-17 18:32:43 +0200

    Slices and borrowing

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 pub(crate) fn main() {
-    let mut a = String::from("Test");
+    let mut mutable_array = [2,4,6,8,10,12,16];
+    let reference_on_array = &mut mutable_array[0..4];
 
     {
-        let b = &mut a;
-        b.push_str(" World!");
+        reference_on_array[0] = 2012;
     }
-    let _c = &a;
-    println!("{a}");
+
+    // println!("{:?}", reference_on_array); // borrow property of mutable_array
+    // println!("{:?}", mutable_array); // mutable_array retrieves its property
+
+    mutable_array[0]=2013;
+
 }
